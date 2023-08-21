1acc3cf667574ab6802f00287d6422a64e34d6a9
Martin Šimek <cima.m@seznam.cz>
2023-08-21 22:44:51 +0200

    Implementation change

--- TheActualImplementation.java
+++ TheActualImplementation.java
@@ -1,9 +1,5 @@
 public class TheActualImplementation implements TheInterface {
     int multiply(int multiplicant, int multiplier) {
-        int product = 0;
-        for(int i = 0; i <= multiplier; i++) {
-            product += multiplicant;
-        }
-        return product;
+        return multiplicant * multiplier;
     }
 }
