5ff9d3cf86e9da5e3915b3ec3d6d5daf92c75c16
Martin Šimek <cima.m@seznam.cz>
2023-08-21 22:48:29 +0200

    Neutral change

--- TheInterface.java
+++ TheInterface.java
@@ -1,3 +1,11 @@
+/** Interface for basic math operation. */
 public interface TheInterface {
+
+    /**
+     * Multiplies two numbers and return product.
+     * 
+     * @param multiplicant first number to be multiplied.
+     * @param multiplier factor of multiplicaiton.
+     */
     int multiply(int multiplicant, int multiplier);
 }
